a2fd2c9d6243d8a668f982d68974f18af665fa98
tan007 <tanli001@gmail.com>
2025-06-12 21:41:21 +0800

    Create JoltGlue.lean

---
+++ TwistShout/Integration/JoltGlue.lean
@@ -0,0 +1,9 @@
+-- Integration/JoltGlue.lean
+import TwistShout.Twist.Core
+import TwistShout.Shout.Core
+namespace TwistShout.Integration
+
+/-- Bridge record tying Twist & Shout to Jolt states (stub). -/
+structure Glue := (dummy : Unit)
+
+end TwistShout.Integration
