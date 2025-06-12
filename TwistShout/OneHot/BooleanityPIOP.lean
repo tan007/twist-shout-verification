19757c67bcf89c1e62cd0ceb78670d11de6d28a6
tan007 <tanli001@gmail.com>
2025-06-12 21:43:04 +0800

    Create BooleanityPIOP.lean

---
+++ TwistShout/OneHot/BooleanityPIOP.lean
@@ -0,0 +1,8 @@
+-- OneHot/BooleanityPIOP.lean
+import TwistShout.OneHot.Encoding
+namespace TwistShout.OneHot
+
+/-- Placeholder for Booleanity PIOP transcript type. -/
+structure BoolPIOP := (dummy : Unit)
+
+end TwistShout.OneHot
