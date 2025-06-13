ac54e22e5e42b9ec5a6a7338e1eb3e260efcb046
tan007 <tanli001@gmail.com>
2025-06-13 18:07:41 +0800

    Update MLE.lean

--- TwistShout/Basic/MLE.lean
+++ TwistShout/Basic/MLE.lean
@@ -1,10 +1,14 @@
--- Basics/MLE.lean
+-- Basics/MLE.lean 
 import Mathlib.Data.Fin.Basic
+import TwistShout.Basics.EqualityFunc
+
 namespace TwistShout.Basics
+open BigOperators
 
 variable {n : ℕ} {𝔽} [Field 𝔽]
 
-/-- Multilinear extension skeleton. -/
-def mle (f : Fin n → Bool) : 𝔽 := 0
+/-- Generic multilinear extension: `mle f x = ∑_{y∈{0,1}ⁿ} f y • eqₑ(y,x)` -/
+def mle (f : (Fin n → Bool) → 𝔽) (x : Fin n → 𝔽) : 𝔽 :=
+  ∑ y : (Fin n → Bool), f y * eqe (fun i ↦ if y i then 1 else 0) x
 
 end TwistShout.Basics
