087a7af08e7fb990c49c41404ff9b365fccd7621
tan007 <tanli001@gmail.com>
2025-06-13 18:08:28 +0800

    Update Encoding.lean

--- TwistShout/OneHot/Encoding.lean
+++ TwistShout/OneHot/Encoding.lean
@@ -1,12 +1,19 @@
 -- OneHot/Encoding.lean
+import TwistShout.Basics.EqualityFunc
 import Mathlib.Algebra.BigOperators
+
 namespace TwistShout.OneHot
 open BigOperators
 
 variable {K : ℕ} {𝔽} [Field 𝔽]
 
-/-- One‑hot predicate. -/
+/-- One‑hot predicate used throughout Shout. -/
 def oneHot (v : Fin K → 𝔽) : Prop :=
-  (∑ i, v i = 1) ∧ ∀ i, (v i)^2 = v i
+  (∑ i, v i = 1) ∧ (∀ i, (v i) ^ 2 = v i)
+
+/-- Direct multilinear encoding `oneHotₑ` (sum‑product form). -/
+def oneHotE (v : Fin K → 𝔽) : 𝔽 :=
+  (∑ i, v i) - 1 + ∑ i, (v i) ^ 2 - v i   -- zero ⇔ v is one‑hot
 
 end TwistShout.OneHot
+
