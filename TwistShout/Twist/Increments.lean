a8b01ea2d67b885eae6f424fe732e63880bda38b
tan007 <tanli001@gmail.com>
2025-06-13 18:09:13 +0800

    Update Increments.lean

--- TwistShout/Twist/Increments.lean
+++ TwistShout/Twist/Increments.lean
@@ -1,10 +1,15 @@
--- Twist/Increments.lean
-import Mathlib.Algebra.GroupPower
+-- Twist/Increments.lean  
+import Mathlib.Algebra.FieldPower
+
 namespace TwistShout.Twist
 
 variable {𝔽} [Field 𝔽]
 
-/-- Increment type. -/
-def Inc := 𝔽
+/-- Core increment relation `Inc = wa * (wv - Val)`. -/
+structure IncRelation :=
+  (K T : ℕ)
+  (wa wv Val Inc : Fin K → Fin T → 𝔽)
+  (h_eq : ∀ k j, Inc k j = wa k j * (wv k j - Val k j))
 
 end TwistShout.Twist
+
